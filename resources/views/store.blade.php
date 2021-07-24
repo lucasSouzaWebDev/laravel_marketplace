eb385a5f66679b259b2e4e64f46643ff4baddaef
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-07-23 21:32:24 -0300

    Página da loja e seus produtos

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -2,7 +2,27 @@
 
 @section('content')
     <div class="row front">
-        @foreach($products as $key => $product)
+        <div class="col-4">
+            @if($store->logo)
+                <img src="{{asset('storage/' . $store->logo)}}" alt="Logo da Loja {{$store->name}}" class="img-fluid">
+            @else                
+                <img src="https://via.placeholder.com/450x100.png?text=logo" alt="Logo sem logo" class="img-fluid">
+            @endif
+        </div>
+        
+        <div class="col-8">
+            <h2>{{$store->name}}</h2>
+            <p>{{$store->description}}</p>
+            <p>
+                <strong>Contatos:</strong>
+                <span>{{$store->phone}}</span> | <span>{{$store->cellphone}}</span>
+            </p>
+        </div>
+        <div class="col-12">
+            <hr>
+            <h3 style="margin-bottom: 30px;">Produtos desta Loja</h3>
+        </div>
+        @forelse($store->products as $key => $product)
             <div class="col-md-4">
                 <div class="card text-center" style="width: 98%;">
                     @if($product->photos->count())
@@ -20,25 +40,11 @@
                     </div>
                 </div>
             </div>
-        @if(($key + 1) % 3 == 0) </div><div class="row front"> @endif
-        @endforeach
-    </div>
-    <div class="row">
-        <div class="col-12 text-center">
-            <h2>Lojas Destaque</h2>
-            <hr>
-        </div>
-        @foreach($stores as $store)
-            <div class="col-4">
-                @if($store->logo)
-                    <img src="{{asset('storage/' . $store->logo)}}" alt="Logo da Loja {{$store->name}}" class="img-fluid">
-                @else                
-                    <img src="https://via.placeholder.com/450x100.png?text=logo" alt="Logo sem logo" class="img-fluid">
-                @endif
-                <h3>{{$store->name}}</h3>
-                <p>{{$store->description}}</p>
-                <a href="{{route('store.single', ['slug' => $store->slug])}}" class="btn btn-sm btn-success">Ver Loja</a>
+            @if(($key + 1) % 3 == 0) </div><div class="row front"> @endif
+        @empty
+            <div class="col-12">
+                <h3 class="alert alert-warning">Nenhum produto encontrado para esta loja!</h3>
             </div>
-        @endforeach
+        @endforelse
     </div>
 @endsection
