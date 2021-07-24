6f6c002b67d3f3ca97c543c22fd23156eed6e8a9
lucasSouzaWebDev <lucasdesouzadubena@gmail.com>
2021-07-24 12:17:51 -0300

    Tela de pedidos do cliente

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::prefix('checkout')->name('checkout.')->group(function(){
 });
 
 Route::group(['middleware' => ['auth']], function(){
+
+    Route::get('my-orders', 'UserOrderController@index')->name('user.orders');
+
     Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
         Route::resource('stores', 'StoreController');
         Route::resource('categories', 'CategoryController');
